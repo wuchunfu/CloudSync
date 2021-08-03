7578e3e95348fb644d790ed0e7918ec95c974182
wuchunfu <319355703@qq.com>
2021-08-03 12:41:44 +0800

    Optimize email notify

--- middleware/notifyx/email/email.go
+++ middleware/notifyx/email/email.go
@@ -1,43 +1,87 @@
 package email
 
 import (
-	"fmt"
-	"github.com/wuchunfu/CloudSync/middleware/configx"
+	"crypto/tls"
 	"gopkg.in/gomail.v2"
 )
 
-type Email struct {
-	EmailSubject string
-	EmailHost    string
-	EmailPort    int
-	EmailUser    string
-	EmailPwd     string
-	ToEmail      string
+// MessageType 内容
+type MessageType struct {
+	From        string
+	To          []string
+	Cc          []string
+	Subject     string
+	ContentType string
+	Content     string
+	Attach      string
 }
 
-func NewEmail(setting *configx.Notify) *Email {
-	subject := setting.Email.EmailSubject
-	host := setting.Email.EmailHost
-	port := setting.Email.EmailPort
-	user := setting.Email.EmailUser
-	pwd := setting.Email.EmailPwd
-	to := setting.Email.ToEmail
-	return &Email{EmailSubject: subject, EmailHost: host, EmailPort: port, EmailUser: user, EmailPwd: pwd, ToEmail: to}
+// ClientType 发送客户端
+type ClientType struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	Message  *MessageType
 }
 
-func (email *Email) Send(body string) error {
-	m := gomail.NewMessage()
-	m.SetHeader("From", "<"+email.EmailUser+">")
-	m.SetHeader("To", []string{email.ToEmail}...)
-	m.SetHeader("Subject", email.EmailSubject)
-	m.SetBody("text/html", body)
-	d := gomail.NewDialer(email.EmailHost, email.EmailPort, email.EmailUser, email.EmailPwd)
-	fmt.Println("正在发送通知...")
-	err := d.DialAndSend(m)
-	if err != nil {
-		fmt.Errorf("邮件发送失败，返回错误: %s", err.Error())
-	} else {
-		fmt.Println("邮件发送成功")
+// NewEmailMessage 返回消息对象
+// from: 发件人
+// subject: 标题
+// contentType: 内容的类型 text/plain text/html
+// attach: 附件
+// to: 收件人
+// cc: 抄送人
+func NewEmailMessage(from string, subject string, contentType string, content string, attach string, to []string, cc []string) *MessageType {
+	return &MessageType{
+		From:        from,
+		Subject:     subject,
+		ContentType: contentType,
+		Content:     content,
+		To:          to,
+		Cc:          cc,
+		Attach:      attach,
 	}
-	return err
+}
+
+// NewEmailClient 返回一个邮件客户端
+// host smtp地址
+// username 用户名
+// password 密码
+// port 端口
+func NewEmailClient(host string, port int, username string, password string, message *MessageType) *ClientType {
+	return &ClientType{
+		Host:     host,
+		Port:     port,
+		Username: username,
+		Password: password,
+		Message:  message,
+	}
+}
+
+// SendMessage 发送邮件
+func (client *ClientType) SendMessage() (bool, error) {
+	dialer := gomail.NewDialer(client.Host, client.Port, client.Username, client.Password)
+	if 587 == client.Port || 465 == client.Port || 994 == client.Port {
+		dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+	dm := gomail.NewMessage()
+	dm.SetHeader("From", client.Message.From)
+	dm.SetHeader("To", client.Message.To...)
+
+	if len(client.Message.Cc) != 0 {
+		dm.SetHeader("Cc", client.Message.Cc...)
+	}
+
+	dm.SetHeader("Subject", client.Message.Subject)
+	dm.SetBody(client.Message.ContentType, client.Message.Content)
+
+	if client.Message.Attach != "" {
+		dm.Attach(client.Message.Attach)
+	}
+
+	if err := dialer.DialAndSend(dm); err != nil {
+		return false, err
+	}
+	return true, nil
 }
--- middleware/notifyx/notify.go
+++ middleware/notifyx/notify.go
@@ -1,20 +1,32 @@
 package notifyx
 
 import (
-	"fmt"
 	"github.com/wuchunfu/CloudSync/middleware/configx"
 	"github.com/wuchunfu/CloudSync/middleware/notifyx/email"
+	"log"
 )
 
 func SendMessage(setting *configx.Notify, msg string) {
 	if setting.IsEnable {
 		// 邮件发送
 		if setting.NotifyType == "email" {
-			newEmail := email.NewEmail(setting)
-			err := newEmail.Send(msg)
+			subject := setting.Email.EmailSubject
+			host := setting.Email.EmailHost
+			port := setting.Email.EmailPort
+			user := setting.Email.EmailUser
+			pwd := setting.Email.EmailPwd
+			to := setting.Email.ToEmail
+			message := email.NewEmailMessage(user, subject, "text/html", msg, "", []string{to}, []string{})
+			client := email.NewEmailClient(host, port, user, pwd, message)
+			ok, err := client.SendMessage()
 			if err != nil {
-				fmt.Println(err)
+				log.Printf("send failed, error: %v", err)
 				return
+			}
+			if ok {
+				log.Println("send successfully")
+			} else {
+				log.Println("send failed!")
 			}
 		}
 	}
