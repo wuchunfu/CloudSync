904e2212cbe338d13f12c86a9f1638c67ed284b8
wuchunfu <319355703@qq.com>
2021-08-04 15:59:56 +0800

    Refactor kv db

--- middleware/database/kvdb/kvdb.go
+++ middleware/database/kvdb/kvdb.go
@@ -1,253 +1,278 @@
 package kvdb
 
 import (
-	"encoding/binary"
-	"errors"
-	"os/user"
-	"strings"
-	"sync"
-	"time"
-
+	"fmt"
+	"github.com/wuchunfu/CloudSync/utils/filex"
 	bolt "go.etcd.io/bbolt"
+	"os"
+	"os/user"
+	"path"
+	"strings"
+	"time"
 )
 
-type KV struct {
-	// Key
-	Key string
-	// Value
-	Val string
-	// TTL(存活时间), 如果设置 ttl = 0 or Persistent, 这个key就会永久不删除
-	TTL time.Duration
+// DBType BlotDB的管理类
+type DBType struct {
+	dbFilePath string
+	bucketName []byte
+	conn       *bolt.DB
 }
 
-type DB struct {
-	filepath string
-	bucket   []byte
-	bOnce    sync.Once
-	conn     *bolt.DB
+type KVInterface interface {
+	// Set 设置值 kv 键值对
+	Set(kv map[string][]byte) error
+	// GetByKey 根据键名获取各自的值, key 键名
+	GetByKey(key string) ([]byte, error)
+	// GetByKeys 根据键名数组获取各自的值, keys 键名数组
+	GetByKeys(keys []string) (map[string][]byte, error)
+	// GetAll 获取全部
+	GetAll() (map[string][]byte, error)
+	// DeleteByKeys 删除键值
+	DeleteByKeys(keys []string) error
+	// DeleteByKeysTransaction 在事务中，删除键值
+	DeleteByKeysTransaction(keys []string) error
+	// DeleteBucket 移除Bucket
+	DeleteBucket(bucketName string) (err error)
+	// Backup 备份数据库文件
+	Backup(backupDbFilePath string) error
+	// Close 关库数据库
+	Close()
 }
 
-type KeyValueStore interface {
-	GetBytes(key string) ([]byte, error)
-	Get(key string) (string, error)
-	SetBytes(key string, val []byte, ttl time.Duration) error
-	Set(key, val string, ttl time.Duration) error
-	TTL(key string) (time.Duration, error)
-	Expire(key string, ttl time.Duration) error
-	Del(key string) error
-	Range() ([]*KV, error)
-}
-
-func NewDB(filepath string) KeyValueStore {
-	if strings.HasPrefix(filepath, "~") {
+// NewKvDb 初始化实例
+func NewKvDb(dbFilePath string, bucketName string) KVInterface {
+	if dbFilePath == "" {
+		return &DBType{}
+	}
+	if strings.HasPrefix(dbFilePath, "~") {
 		u, err := user.Current()
 		if err != nil {
 			panic(err)
 		}
-		filepath = u.HomeDir + filepath[1:]
+		dbFilePath = u.HomeDir + dbFilePath[1:]
 	}
-	return &DB{
-		filepath: filepath,
-		bucket:   []byte("kv-bucket"),
+	dirName := path.Dir(dbFilePath)
+	if !filex.FilePathExists(dirName) {
+		err := os.MkdirAll(dirName, os.ModePerm)
+		if err != nil {
+			fmt.Errorf("create dir(%s) error: %s", dirName, err)
+			return &DBType{}
+		}
+	}
+	return &DBType{
+		dbFilePath: dbFilePath,
+		bucketName: []byte(bucketName),
 	}
 }
 
-func (dbase *DB) newConn() error {
+// newConn 创建库管理, 并生成Bucket
+func (dbase *DBType) newConn() error {
 	if dbase.conn == nil {
-		db, err := bolt.Open(dbase.filepath, 0600, nil)
-		if err != nil {
-			return err
+		db, openErr := bolt.Open(dbase.dbFilePath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+		if openErr != nil {
+			return fmt.Errorf("open db error: %s", openErr)
 		}
+		err := db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists(dbase.bucketName)
+			if err != nil {
+				return fmt.Errorf("create bucket error: %s", err)
+			}
+			return nil
+		})
 		dbase.conn = db
+		return err
 	}
 	return nil
 }
 
-func (dbase *DB) GetBytes(key string) ([]byte, error) {
-	ttl, result, err := dbase.getWithExpire(key)
-	if err != nil {
-		return nil, err
-	} else if ttl < 0 {
-		return nil, nil
+// Set 设置值 kv 键值对
+func (dbase *DBType) Set(kv map[string][]byte) error {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return connErr
 	}
-	return result, nil
-}
 
-func (dbase *DB) Get(key string) (string, error) {
-	ttl, result, err := dbase.getWithExpire(key)
-	if err != nil {
-		return "", err
-	} else if ttl < 0 {
-		return "", nil
-	}
-	return string(result), nil
-}
-
-func (dbase *DB) SetBytes(key string, val []byte, ttl time.Duration) error {
-	if err := dbase.newConn(); err != nil {
-		return err
-	}
-	return dbase.conn.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists(dbase.bucket)
-		if err != nil {
-			return err
-		}
-		buf := make([]byte, 8+len(val))
-		binary.PutVarint(buf[:8], toMillisecond(ttl))
-		copy(buf[8:], val)
-		return b.Put([]byte(key), buf)
-	})
-}
-
-func (dbase *DB) Set(key, val string, ttl time.Duration) error {
-	if err := dbase.newConn(); err != nil {
-		return err
-	}
-	return dbase.conn.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists(dbase.bucket)
-		if err != nil {
-			return err
-		}
-		buf := make([]byte, 8+len(val))
-		binary.PutVarint(buf[:8], toMillisecond(ttl))
-		copy(buf[8:], val)
-		return b.Put([]byte(key), buf)
-	})
-}
-
-func (dbase *DB) TTL(key string) (time.Duration, error) {
-	ttl, _, err := dbase.getWithExpire(key)
-	if err != nil {
-		return -1, err
-	} else if ttl < -1 {
-		return -1, nil
-	}
-	return time.Duration(ttl) * time.Millisecond, nil
-}
-
-func (dbase *DB) Expire(key string, ttl time.Duration) error {
-	_, result, err := dbase.getWithExpire(key)
-	if err != nil {
-		return err
-	} else if ttl < -1 {
-		return errors.New("key expired")
-	}
-	return dbase.Set(key, string(result), ttl)
-}
-
-func (dbase *DB) Del(key string) error {
-	if err := dbase.newConn(); err != nil {
-		return err
-	}
-	return dbase.conn.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists(dbase.bucket)
-		if err != nil {
-			return err
-		}
-		return b.Delete([]byte(key))
-	})
-}
-
-func (dbase *DB) Range() ([]*KV, error) {
-	if err := dbase.newConn(); err != nil {
-		return nil, err
-	}
-	var kvs []*KV
-	if err := dbase.conn.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(dbase.bucket)
-		if b == nil {
-			return nil
-		}
-		return b.ForEach(func(k, v []byte) error {
-			expiredAt, err := binaryInt(v[:8])
+	return dbase.conn.Batch(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(dbase.bucketName)
+		var err error
+		for k, v := range kv {
+			err = bucket.Put([]byte(k), v)
 			if err != nil {
 				return err
 			}
-			ttl := expiredAt - int(time.Now().UnixNano()/int64(1000000))
-			if ttl < 0 {
-				// 过期了, 删除
-				err := dbase.conn.Update(func(tx *bolt.Tx) error {
-					b, err := tx.CreateBucketIfNotExists(dbase.bucket)
-					if err != nil {
-						return err
-					}
-					return b.Delete(k)
-				})
-				if err != nil {
-					return err
-				}
-				return nil
-			}
-			kvs = append(kvs, &KV{
-				Key: string(k),
-				Val: string(v[8:]),
-				TTL: time.Duration(ttl) * time.Millisecond,
-			})
-			return nil
-		})
-	}); err != nil {
-		return nil, err
-	}
-	return kvs, nil
+		}
+		return err
+	})
 }
 
-func (dbase *DB) getOriginData(key string) ([]byte, error) {
-	if err := dbase.newConn(); err != nil {
-		return nil, err
+// GetByKey 根据键名获取各自的值, key 键名
+func (dbase *DBType) GetByKey(key string) ([]byte, error) {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return nil, connErr
 	}
-	var result []byte
-	if err := dbase.conn.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(dbase.bucket)
-		if b == nil {
-			return nil
-		}
-		result = b.Get([]byte(key))
+
+	var value []byte
+	err := dbase.conn.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(dbase.bucketName)
+		result := bucket.Get([]byte(key))
+		tmp := make([]byte, len(result))
+		copy(tmp, result)
+		value = tmp
 		return nil
-	}); err != nil {
-		return nil, err
+	})
+	if err != nil {
+		return nil, fmt.Errorf("GetByKey error: %s", err)
 	}
-	return result, nil
+	return value, nil
 }
 
-func (dbase *DB) getWithExpire(key string) (int, []byte, error) {
-	result, err := dbase.getOriginData(key)
-	if err != nil {
-		return -1, nil, nil
-	} else if result == nil {
-		return -1, nil, nil
+// GetByKeys 根据键名数组获取各自的值, keys 键名数组
+func (dbase *DBType) GetByKeys(keys []string) (map[string][]byte, error) {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return nil, connErr
 	}
-	expiredAt, err := binaryInt(result[:8])
+
+	values := make(map[string][]byte)
+	err := dbase.conn.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(dbase.bucketName)
+		for _, k := range keys {
+			result := bucket.Get([]byte(k))
+			if result == nil {
+				continue
+			}
+			tmp := make([]byte, len(result))
+			copy(tmp, result)
+			values[k] = tmp
+		}
+		return nil
+	})
 	if err != nil {
-		return -1, nil, err
+		return nil, fmt.Errorf("GetByKeys error: %s", err)
 	}
-	ttl := expiredAt - int(time.Now().UnixNano()/int64(1000000))
-	if ttl < 0 {
-		// 过期了, 删除
-		err := dbase.conn.Update(func(tx *bolt.Tx) error {
-			b, err := tx.CreateBucketIfNotExists(dbase.bucket)
+	return values, nil
+}
+
+// GetAll 获取全部
+func (dbase *DBType) GetAll() (map[string][]byte, error) {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return nil, connErr
+	}
+
+	values := make(map[string][]byte)
+	err := dbase.conn.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(dbase.bucketName)
+		err := bucket.ForEach(func(k, v []byte) error {
+			tmpK := make([]byte, len(k))
+			copy(tmpK, k)
+			tmpV := make([]byte, len(v))
+			copy(tmpV, v)
+			values[string(tmpK)] = tmpV
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("GetAll error: %s", err)
+	}
+	return values, nil
+}
+
+// DeleteByKeys 删除键值
+func (dbase *DBType) DeleteByKeys(keys []string) error {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return connErr
+	}
+
+	return dbase.conn.Batch(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(dbase.bucketName)
+		var err error
+		for _, key := range keys {
+			err = bucket.Delete([]byte(key))
+			if err != nil {
+				return fmt.Errorf("DeleteByKeys error: %s", err)
+			}
+		}
+		return err
+	})
+}
+
+// DeleteByKeysTransaction 在事务中，删除键值
+func (dbase *DBType) DeleteByKeysTransaction(keys []string) error {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return connErr
+	}
+
+	tx, txErr := dbase.conn.Begin(true)
+	if txErr != nil {
+		return txErr
+	}
+	bucket := tx.Bucket(dbase.bucketName)
+	for _, key := range keys {
+		err := bucket.Delete([]byte(key))
+		if err != nil {
+			err := tx.Rollback()
 			if err != nil {
 				return err
 			}
-			return b.Delete([]byte(key))
-		})
-		if err != nil {
-			return -1, nil, err
+			return fmt.Errorf("DeleteByKeysTransaction error: %s", err)
 		}
-		return -1, nil, err
 	}
-	return ttl, result[8:], nil
+	err := tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func toMillisecond(ttl time.Duration) int64 {
-	return time.Now().Add(ttl).UnixNano() / int64(1000000)
+// DeleteBucket 删除Bucket
+func (dbase *DBType) DeleteBucket(bucketName string) error {
+	connErr := dbase.newConn()
+	if connErr != nil {
+		return connErr
+	}
+
+	err := dbase.conn.Update(func(tx *bolt.Tx) error {
+		return tx.DeleteBucket([]byte(bucketName))
+	})
+	if err != nil {
+		return fmt.Errorf("DeleteBucket error: %s", err)
+	}
+	return err
 }
 
-func binaryInt(buf []byte) (int, error) {
-	x, n := binary.Varint(buf)
-	if n == 0 {
-		return 0, errors.New("buf too small")
-	} else if n < 0 {
-		return 0, errors.New("value larger than 64 bits (overflow) and -n is the number of bytes read")
+// Backup 备份数据库文件
+func (dbase *DBType) Backup(backupDbFilePath string) error {
+	db, err := bolt.Open(dbase.dbFilePath, 0600, nil)
+	if err != nil {
+		return fmt.Errorf("open db error: %s", err)
 	}
-	return int(x), nil
+	err = db.View(func(tx *bolt.Tx) error {
+		err := tx.CopyFile(backupDbFilePath, 0644)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("backup error: %s", err)
+	}
+	return db.Close()
+}
+
+// Close 关库数据库
+func (dbase *DBType) Close() {
+	func(conn *bolt.DB) {
+		err := conn.Close()
+		if err != nil {
+			return
+		}
+	}(dbase.conn)
 }
