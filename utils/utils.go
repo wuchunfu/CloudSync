cb7c685c542c17e614020330f23cd95df6c17853
wuchunfu <319355703@qq.com>
2021-08-10 15:58:47 +0800

    Add utils

--- utils/utils.go
+++ utils/utils.go
@@ -2,9 +2,26 @@ package utils
 
 import (
 	"math/rand"
+	"os"
+	"os/user"
 	"strings"
 	"time"
 )
+
+// CurrentUser 获取当前SSH连接的用户
+func CurrentUser() string {
+	currentUser, _ := user.Current()
+	return currentUser.Username
+}
+
+// UserHome 获取用户的家目录
+func UserHome() (string, error) {
+	currentUser, err := user.Current()
+	if err == nil {
+		return currentUser.HomeDir, nil
+	}
+	return os.Getenv("HOME"), nil
+}
 
 // IgnoreFile Check if the file is contains the ignore file
 func IgnoreFile(ignoreFiles []string, filename string) bool {
@@ -26,4 +43,44 @@ func RandomString(n int) string {
 		result[i] = byteStr[rand.Intn(len(byteStr))]
 	}
 	return string(result)
+}
+
+// GenRandomString 生成随机字符串
+// length 生成长度
+// specialChar 是否生成特殊字符
+func GenRandomString(length int, specialChar bool) string {
+	letterBytes := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	special := "!@#%$*.="
+
+	if specialChar {
+		letterBytes = letterBytes + special
+	}
+	chars := []byte(letterBytes)
+	if length == 0 {
+		return ""
+	}
+
+	clen := len(chars)
+	maxChar := 255 - (256 % clen)
+	b := make([]byte, length)
+	// storage for random bytes.
+	r := make([]byte, length+(length/4))
+	i := 0
+	for {
+		if _, err := rand.Read(r); err != nil {
+			return ""
+		}
+		for _, rb := range r {
+			c := int(rb)
+			if c > maxChar {
+				// Skip this number to avoid modulo bias.
+				continue
+			}
+			b[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(b)
+			}
+		}
+	}
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,8 +3,34 @@ package utils
 import "testing"
 
 func TestUtils(t *testing.T) {
-	ignoreFiles := []string{".git", ".idea", ".swp", ".swx"}
-	fileName := "/tmp/test.swp"
-	file := IgnoreFile(ignoreFiles, fileName)
-	t.Log(file)
+
+	t.Run("IgnoreFiles", func(t *testing.T) {
+		ignoreFiles := []string{".git", ".idea", ".swp", ".swx"}
+		fileName := "/tmp/test.swp"
+		file := IgnoreFile(ignoreFiles, fileName)
+		t.Log(file)
+	})
+
+	t.Run("CurrentUser", func(t *testing.T) {
+		user := CurrentUser()
+		t.Log(user)
+	})
+
+	t.Run("UserHome", func(t *testing.T) {
+		home, err := UserHome()
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(home)
+	})
+
+	t.Run("RandomString", func(t *testing.T) {
+		randomString := RandomString(12)
+		t.Log(randomString)
+	})
+
+	t.Run("GenRandomString", func(t *testing.T) {
+		randomString := GenRandomString(12, true)
+		t.Log(randomString)
+	})
 }
